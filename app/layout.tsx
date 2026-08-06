540d40bbaa695b332e29db25865344d2e2bef9e5
Jonathan Kashongwe <jokashongwe@gmail.com>
2026-08-06 12:46:56 +0100

    Simple App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
+import ServiceWorkerRegister from "../ServiceWorkerRegister";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Masomo",
-  description: "Application de gestion scolaire",
+  title: "KelaApp",
+  description: "Application de gestion scolaire KelaApp",
 };
 
 export default function RootLayout({
@@ -27,7 +27,18 @@ export default function RootLayout({
       lang="fr"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans antialiased">{children}</body>
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="KelaApp" />
+        <meta name="msapplication-TileColor" content="#2D9CDB" />
+        <meta name="theme-color" content="#2D9CDB" />
+      </head>
+      <body className="min-h-full flex flex-col font-sans antialiased">
+        {children}
+        <ServiceWorkerRegister />
+      </body>
     </html>
   );
 }
